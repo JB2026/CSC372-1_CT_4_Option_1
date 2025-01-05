de37f2c7c8ae4bd781a29c00d23349f8c9ebe132
Joshua Borck <joshua.borck@csuglobal.edu>
2025-01-05 11:37:54 -0500

    Handling some revisions

--- src/ShapeArray.java
+++ src/ShapeArray.java
@@ -1,25 +1,25 @@
+import java.util.ArrayList;
 
 // Class to that creates shapes and outputs their information
 public class ShapeArray {
 
 	public static void main(String[] args) {
 		// Create the shape array
-		Shape[] shapeArray = new Shape[3];
-		
+		ArrayList<Shape> shapes = new ArrayList<Shape>();
+
 		// Create the shapes
 		Sphere sphere = new Sphere(14.0);
 		Cylinder cylinder = new Cylinder(11.0, 16.0);
 		Cone cone = new Cone(12.0, 15.0);
-		
+
 		// Add the shapes to the shapeArray
-		shapeArray[0] = sphere;
-		shapeArray[1] = cylinder;
-		shapeArray[2] = cone;
+		shapes.add(sphere);
+		shapes.add(cylinder);
+		shapes.add(cone);
 
 		// Loop through and get the toString output of each shape
-		for (int i = 0; i < shapeArray.length; i++) {
-			Shape currentShape = shapeArray[i];
-			System.out.println(currentShape.toString());
+		for (Shape shape: shapes) {
+			System.out.println(shape.toString());
 		}
 	}
 }
